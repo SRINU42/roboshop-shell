3aa73715a1a51dfff96180a7958c229d79f72690
SRINU <vasu_anand01@yahoo.co.in>
2023-05-31 17:42:48 +0530

    log file added

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,17 @@
 
 echo -e "\e[33m installing nginx \e[0m"
-yum install nginx -y 
+yum install nginx -y >/tem/roboshop.log
+
 echo -e "\e[33m removing content in nginx \e[0m"
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* >/tem/roboshop.log
+
 echo -e "\e[33m loading the content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tem/roboshop.log
+
 echo -e "\e[33m unzizng the file\e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >/tem/roboshop.log
+
 echo -e "\e[33m starting nginx server \e[0m"
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx >/tem/roboshop.log
+systemctl start nginx >/tem/roboshop.log
