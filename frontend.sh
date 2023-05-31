a62a483b5be613329f7312218c564ec55a89211b
SRINU <vasu_anand01@yahoo.co.in>
2023-05-31 17:49:43 +0530

    new commit

--- frontend.sh
+++ frontend.sh
@@ -6,12 +6,12 @@ echo -e "\e[33m removing content in nginx \e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[33m loading the content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log
 
 echo -e "\e[33m unzizng the file\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip >/tmp/roboshop.log
 
 echo -e "\e[33m starting nginx server \e[0m"
-systempctl enable nginx >/tmp/roboshop.log
+systemctl enable nginx >/tmp/roboshop.log
 systemctl start nginx >/tmp/roboshop.log
