a0f82324bf58a79ec61589ecf810f5ea27ad8258
SRINU <vasu_anand01@yahoo.co.in>
2023-05-31 20:03:48 +0530

    sed commmand

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,12 @@
 # copy the repo 
 echo -e "\e[33m  mongo repo file \e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+
 echo -e "\e[33m installing mongo \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
-# modify the ip adderss
+echo -e "\e[33m update monogodb listen adderss \e[0m"
+sed -i -e 'S/127.0.0.1/0.0.0.0/' /etc/mongod.conf&>>/tmp/roboshop.log
 
 echo -e "\e[33m start the mogodb \e[0m"
 systemctl restart mongod &>>/tmp/roboshop.log
