0d75c093454e2bc01e53e633829241e8bc5edf46
SRINU <vasu_anand01@yahoo.co.in>
2023-05-31 19:17:48 +0530

    update

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 # copy the repo 
 echo -e "\e[33m  mongo repo file \e[0m"
-cp mongodb.repo /etc/yum.repo.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[33m installing mongo \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
