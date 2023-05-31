c32bc1127a67d88efa7c697f9b02745be4fca1a6
SRINU <vasu_anand01@yahoo.co.in>
2023-05-31 20:09:04 +0530

    add

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ echo -e "\e[33m installing mongo \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m update monogodb listen adderss \e[0m"
-sed -i -e 'S/127.0.0.1/0.0.0.0/' /etc/mongod.conf&>>/tmp/roboshop.log
+sed -i -e 'S/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
 
 echo -e "\e[33m start the mogodb \e[0m"
 systemctl restart mongod &>>/tmp/roboshop.log
