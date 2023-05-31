e52e48dfb7523aceaee601114b6f3e347ff653d9
SRINU <vasu_anand01@yahoo.co.in>
2023-05-31 17:34:26 +0530

    colour code is added

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,13 @@
+
+echo -e "\e[33m installing nginx \e[0m"
 yum install nginx -y 
-
+echo -e "\e[33m removing content in nginx \e[0m"
 rm -rf /usr/share/nginx/html/* 
-
+echo -e "\e[33m loading the content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-
+echo -e "\e[33m unzizng the file\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-
+echo -e "\e[33m starting nginx server \e[0m"
 systemctl enable nginx 
 systemctl start nginx 
