4a6e73d895e5202fb308a328fa68ddecf2fed423
SRINU <vasu_anand01@yahoo.co.in>
2023-06-01 22:08:36 +0530

    2

--- mongodb.sh
+++ mongodb.sh
@@ -8,7 +8,8 @@ echo -e "\e[33m update mongodb lisiten address \e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
 
 echo -e "\e[33m start mongodb \e[0m"
-systemctl enable mongod 
-systemctl restart mongod  &>>/tmp/roboshop.log
+systemctl enable mongod &>>/tmp/roboshop.log
+ 
+systemctl restart mongod &>>/tmp/roboshop.log
 
 
