77cdfbcdd6c080c24bea7ac0fdb319aae256873d
SRINU <vasu_anand01@yahoo.co.in>
2023-06-01 15:20:13 +0530

    one update

--- catalogue.sh
+++ catalogue.sh
@@ -17,7 +17,7 @@ cd /app
 unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33m install apllication content \e[0m"
-npm install 
+npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33m setup conf \e[0m"
 cp /home/centos/roboshop-shell/catalogue.service vim /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
