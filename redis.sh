de396e71fb6995ec7e70b7fca182c5f1dfaeb23e
SRINU <vasu_anand01@yahoo.co.in>
2023-06-01 17:43:00 +0530

    new

--- redis.sh
+++ redis.sh
@@ -1,12 +1,12 @@
 echo -e "\e[33m conf redis repo \e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m enavle redis module \e[0m"
-yum module enable redis:remi-6.2 -y
+yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m installing redis \e[0m"
-yum install redis -y 
+yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m start redis \e[0m"
 systemctl enable redis 
-systemctl start redis 
+systemctl start redis &>>/tmp/roboshop.log
