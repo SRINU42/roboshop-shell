a400a363bc84be62901441291c564e1e5ab7daa3
SRINU <vasu_anand01@yahoo.co.in>
2023-06-01 20:05:54 +0530

    mongodb

---
+++ mongodb.sh
@@ -0,0 +1,14 @@
+echo -e "\e[33m dwonloading mongo repo fiel \e[0m"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33m installing mongodb server \e[0m"
+yum install mongodb-org -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m update mongodb lisiten adders \e[0m"
+sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>/tmp/roboshop.log
+
+echo -e "\e[33m start mongodb \e[0m"
+systemctl enable mongod 
+systemctl start mongod &>>/tmp/roboshop.log
+
+
