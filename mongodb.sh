db8951fdbe76b3b9d4fe4904b1d9287eaa32177c
SRINU <vasu_anand01@yahoo.co.in>
2023-06-01 21:57:23 +0530

    a

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[33m installing mongodb server \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m update mongodb lisiten address \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0'a /etc/mongod.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m start mongodb \e[0m"
 systemctl enable mongod 
