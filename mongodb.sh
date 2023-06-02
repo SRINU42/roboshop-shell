94e1a8d9734c867a06342fc21f2d9cb11350c4a9
SRINU <vasu_anand01@yahoo.co.in>
2023-06-02 20:50:46 +0530

    a

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,12 @@
-echo -e "\e[33m install nginx \e[0m"
+echo -e "\e[33m configuring mongodb repo \e[0m"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33m install nginx \e[0m"
+echo -e "\e[33m install mongodb \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m install nginx \e[0m"
+echo -e "\e[33m changing the cong lisening address \e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 
-echo -e "\e[33m install nginx \e[0m"
+echo -e "\e[33m install start mongodb \e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
