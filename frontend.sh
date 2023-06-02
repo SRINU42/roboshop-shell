1c7ee9af04ffc3c9ac4420e153f1b0e43e58c0ec
SRINU <vasu_anand01@yahoo.co.in>
2023-06-02 19:09:13 +0530

    a

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "\e[33m installing nginx \e[0m"
+echo -e "\e[33m install nginx \e[0m"
 yum install nginx -y &>>/tmp/robokshop.log
 
-echo -e "\e[33m remove content nginx \e[0m"
+echo -e "\e[33m removing content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/robokshop.log
 
-echo -e "\e[33m dowloading the content \e[0m"
+echo -e "\e[33m dowloading content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/robokshop.log
 
-echo -e "\e[33m unzip the content \e[0m"
+echo -e "\e[33m unzip content \e[0m"
 cd /usr/share/nginx/html &>>/tmp/robokshop.log
 unzip /tmp/frontend.zip &>>/tmp/robokshop.log
 
-echo -e "\e[33m fetching the conf \e[0m"
+echo -e "\e[33m conf services \e[0m"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/robokshop.log
 
-echo -e "\e[33m start nginx \e[0m"
-systemctl enable nginx 
-systemctl restart nginx  
+echo -e "\e[33m starting \e[0m"
+systemctl enable nginx &>>/tmp/robokshop.log
+systemctl restart nginx &>>/tmp/robokshop.log
