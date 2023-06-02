2f9b07558ee69085a36ab0699e28ce4ddf8434c6
SRINU <vasu_anand01@yahoo.co.in>
2023-06-02 09:56:40 +0530

    mongodb

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,20 @@
-yum install nginx -y 
+echo -e "\e[33m installing nginx \e[0m"
+yum install nginx -y &>>/tmp/robokshop.log
 
-rm -rf /usr/share/nginx/html/* 
+echo -e "\e[33m remove content nginx \e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/robokshop.log
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+echo -e "\e[33m dowloading the content \e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/robokshop.log
 
-
+echo -e "\e[33m unzip the content \e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/robokshop.log
 
-# We the copy the conf file
+echo -e "\e[33m fetching the conf \e[0m"
+cp /home/centos/roboshop-shell/ &>>/tmp/robokshop.log
 
-
+echo -e "\e[33m start nginx \e[0m"
 systemctl enable nginx 
-systemctl restart nginx  
+systemctl restart nginx  &>>/tmp/robokshop.log
 
---
+++ mongodb.sh
@@ -0,0 +1,12 @@
+echo -e "\e[33m mogodb repofile \e[0m"
+cp mongo.rep /etc/yum.repos.d/mongo.repo &>>/tmp/robokshop.log
+
+echo -e "\e[33m install mongodb \e[0m"
+yum install mongodb-org -y &>>/tmp/robokshop.log
+
+echo -e "\e[33m Upadte monkgodb listen \e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+echo -e "\e[33m start mongodb \e[0m"
+systemctl enable mongod 
+systemctl restart mongod &>>/tmp/robokshop.log
