470e215f3a8df72c7e54d93cd225d9500df3da7a
SRINU <vasu_anand01@yahoo.co.in>
2023-06-02 20:18:26 +0530

    a

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "\e[33m install nginx \e[03"
+echo -e "\e[33m install nginx \e[0m"
 yum install nginx -y 
 
-echo -e "\e[33m removie content \e[03"
+echo -e "\e[33m removie content \e[0m"
 rm -rf /usr/share/nginx/html/* 
 
-echo -e "\e[33m dwonload content \e[03"
+echo -e "\e[33m dwonload content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
-echo -e "\e[33m unzip the content \e[03"
+echo -e "\e[33m unzip the content \e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-echo -e "\e[33m start nginx \e[03"
+echo -e "\e[33m start nginx \e[0m"
 #cp 
  
-echo -e "\e[33m start nginx \e[03"
+echo -e "\e[33m start nginx \e[0m"
 systemctl enable nginx 
 systemctl restart nginx 
