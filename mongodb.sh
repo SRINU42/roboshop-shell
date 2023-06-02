5aec8de3ab8ede16cfbb4c9c9d0450454e15e094
SRINU <vasu_anand01@yahoo.co.in>
2023-06-02 11:18:28 +0530

    a

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[33m install mongodb \e[0m"
 yum install mongodb-org -y &>>/tmp/robokshop.log
 
 echo -e "\e[33m Upadte monkgodb listen \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33m start mongodb \e[0m"
 systemctl enable mongod 
